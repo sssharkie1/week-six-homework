13dac505e79917fafbe0400c87cc1436bb2d5a73
Sarah Stevens <sssharkie1>
2016-12-26 10:18:45 -0500

    buttons work

--- javascript.js
+++ javascript.js
@@ -1,49 +1,55 @@
-var topics = ["cat", "elephant", "mule", "guinea hen"]
 
-function alertMovieName() {
 
-				
+			var topics = ["cat", "elephant", "mule", "guinea hen"];
 
-					// YOUR CODE GOES HERE!!!
+			// Function for dumping the JSON content for each button into the div
+			function displayTopicInfo() {
 
+				// YOUR CODE GOES HERE!!! HINT: You will need to create a new div to hold the JSON.
+
+			}
+
+			// Function for displaying movie data
+			function renderButtons() {
+
+				// Deleting the buttons prior to adding new movies
+				// (this is necessary otherwise you will have repeat buttons)
+				$("#buttons-view").empty();
+
+				// Looping through the array of movies
+				for (var i = 0; i < topics.length; i++) {
+
+					// Then dynamicaly generating buttons for each movie in the array
+					// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+					var a = $("<button>");
+					// Adding a class of movie to our button
+					a.addClass("topic");
+					// Adding a data-attribute
+					a.attr("data-name", topics[i]);
+					// Providing the initial button text
+					a.text(topics[i]);
+					// Adding the button to the buttons-view div
+					$("#buttons-view").append(a);
 				}
+			}
 
+			// This function handles events where one button is clicked
+			$("#add-topic").on("click", function(event) {
+				event.preventDefault();
 
+				// This line grabs the input from the textbox
+				var topic = $("#topic-input").val().trim();
 
-				function renderButtons() {
+				// The movie from the textbox is then added to our array
+				topics.push(topic);
 
-					// Deleting the movies prior to adding new movies
-					// (this is necessary otherwise we will have repeat buttons)
-					$("#addTopic").empty();
+				// Calling renderButtons which handles the processing of our movie array
+				renderButtons();
 
-					// Looping through the array of movies
-					for (var i = 0; i < topics.length; i++) {
+			});
 
-						// Then dynamicaly generating buttons for each movie in the array
-						// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-						var a = $("<button>");
-						// Adding a class
-						a.addClass("topic");
-						// Added a data-attribute
-						a.attr("data-name", topics[i]);
-						// Provided the initial button text
-						a.text(topics[i]);
-						// Added the button to the HTML
-						$("#addTopic").append(a);
-					}
-				}
+			// Generic function for displaying the movieInfo
+			$(document).on("click", ".topic", displayTopicInfo);
 
-				$("#addTopic").on("click", function(event) {
-					event.preventDefault();
-
-					// This line grabs the input from the textbox
-					var topics = $("#topicInput").val().trim();
-
-					// The topics from the textbox is then added to our array
-					topics.push(topics);
-
-					// Calling renderButtons which handles the processing of our movie array
-					renderButtons();
-				});
-
-
+			// Calling the renderButtons function to display the intial buttons
+			renderButtons();
