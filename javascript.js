99b8e5f3fe9623c95d0954358ebd4364b0a86698
Sarah Stevens <sssharkie1>
2016-12-27 10:16:51 -0500

    still and animate added

--- javascript.js
+++ javascript.js
@@ -1,55 +1,84 @@
 
 
-			var topics = ["cat", "elephant", "mule", "guinea hen"];
+	var topics = ["elephant","crow", "guinea hen"];
 
-			// Function for dumping the JSON content for each button into the div
-			function displayTopicInfo() {
 
-				// YOUR CODE GOES HERE!!! HINT: You will need to create a new div to hold the JSON.
+	function renderButtons() {
 
-			}
-
-			// Function for displaying movie data
-			function renderButtons() {
-
-				// Deleting the buttons prior to adding new movies
-				// (this is necessary otherwise you will have repeat buttons)
-				$("#buttons-view").empty();
-
-				// Looping through the array of movies
 				for (var i = 0; i < topics.length; i++) {
-
-					// Then dynamicaly generating buttons for each movie in the array
-					// This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
 					var a = $("<button>");
-					// Adding a class of movie to our button
-					a.addClass("topic");
-					// Adding a data-attribute
-					a.attr("data-name", topics[i]);
-					// Providing the initial button text
+					a.addClass("topicClass");
+					a.data("data-topicSearch", topics[i]);
 					a.text(topics[i]);
-					// Adding the button to the buttons-view div
 					$("#buttons-view").append(a);
 				}
-			}
+	}
 
-			// This function handles events where one button is clicked
 			$("#add-topic").on("click", function(event) {
 				event.preventDefault();
-
-				// This line grabs the input from the textbox
 				var topic = $("#topic-input").val().trim();
-
-				// The movie from the textbox is then added to our array
 				topics.push(topic);
 
-				// Calling renderButtons which handles the processing of our movie array
-				renderButtons();
+			$("#topic-form").val('');
+        	$("#buttons-view").empty();
+        	renderButtons();
 
 			});
 
-			// Generic function for displaying the movieInfo
-			$(document).on("click", ".topic", displayTopicInfo);
-
-			// Calling the renderButtons function to display the intial buttons
+			$(document).on("click", ".topicClass", displayTopicInfo);
 			renderButtons();
+
+			function displayTopicInfo() {
+
+
+        var topic = $(this).data("topicSearch");
+      	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+        topics + "&api_key=dc6zaTOxFJmzC&limit=1";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET",
+        })
+          	.done(function(response) {
+
+          var results = response.data;
+          for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>");
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var topicImage = $("<img>");
+            topicImage.attr("src", results[i].images.fixed_height.url);
+
+            //added these
+            topicImage.attr("data-still", results[i].images.fixed_height_still.url);
+            topicImage.attr("data-animate", results[i].images.fixed_height.url);
+            topicImage.attr("data-state", "still");
+            topicImage.addClass("gif");
+
+            //end of added
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(topicImage);
+            $("#gifs-appear-here").prepend(gifDiv);
+          }
+        	})
+
+    }
+
+      $(document).on("click", ".gif", function(e){
+
+      var state = $(this).attr("data-state");
+      console.log(state);
+        if (state === "still"){
+          var newSource = $(this).attr("data-animate");
+          $(this).attr("src",newSource);
+          $(this).attr("data-state","animate");
+      }
+      else if (state==="animate"){
+          var newSource = $(this).attr("data-still");
+          $(this).attr("src",newSource);
+          $(this).attr("data-state","still");
+      }
+    // app.imageClicked(e);
+
+  });
